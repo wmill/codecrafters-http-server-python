7a761a3a5e0f644776fa4c88a9efb922cebcd029
Walter Miller <waltermil@gmail.com>
2024-02-15 18:26:52 -0500

    stage 6 file download

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
 # Uncomment this to pass the first stage
 import socket
 import threading
+import argparse
+
+global_args = {}
 
 def response(status_code, content_type, body):
     response = "HTTP/1.1 " + status_code + "\r\n"
     response += "Content-Type: " + content_type + "\r\n"
     response += "Content-Length: " + str(len(body)) + "\r\n"
     response += "\r\n"
+    response = response.encode("ascii")
     response += body
     return response
 
@@ -16,8 +20,9 @@ def handle_request(client_socket):
     if not data:
         client_socket.close()
         return
-    stringData = data.decode("ascii")
-    lines = stringData.split("\r\n")
+
+    headerData, bodyData = data.split(b"\r\n\r\n")
+    lines = headerData.decode("ascii").split("\r\n")
     verb, path, protocol = lines[0].split(" ")
     headers = {}
     for line in lines[1:]:
@@ -30,9 +35,16 @@ def handle_request(client_socket):
             client_socket.send("HTTP/1.1 200 OK\r\n\r\nHello, World!".encode("ascii"))
         elif path.startswith("/echo"):
             #server_socket.send(f"HTTP/1.1 200 OK\r\n\r\n{path[6:]}".encode("ascii"))
-            client_socket.send(response("200 OK", "text/plain", path[6:]).encode("ascii"))
+            client_socket.send(response("200 OK", "text/plain", path[6:].encode("ascii")))
         elif path.startswith("/user-agent"):
-            client_socket.send(response("200 OK", "text/plain", headers["User-Agent"]).encode("ascii"))
+            client_socket.send(response("200 OK", "text/plain", headers["User-Agent"].encode("ascii")))
+        elif path.startswith("/files"):
+            try:
+                filename = path[len("/files"):]
+                with open(global_args.directory + filename, "rb") as file:
+                    client_socket.send(response("200 OK", "application/octet-stream", file.read()))
+            except FileNotFoundError:
+                client_socket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode("ascii"))
         else:
             client_socket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode("ascii"))
     # server_socket.send("HTTP/1.1 200 OK\r\n\r\n".encode("ascii"))
@@ -60,4 +72,9 @@ def main():
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Toy HTTP server")
+    parser.add_argument("--directory", type=str, help="Directory to serve static files from", default="./files")
+    args = parser.parse_args()
+    global_args = args
+
     main()
