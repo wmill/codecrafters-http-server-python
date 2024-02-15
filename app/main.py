3dbc3704572800b720fe9651db10724674663f56
Walter Miller <waltermil@gmail.com>
2024-02-15 15:26:34 -0500

    stage 6 multithreaded requests

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 # Uncomment this to pass the first stage
 import socket
+import threading
 
 def response(status_code, content_type, body):
     response = "HTTP/1.1 " + status_code + "\r\n"
@@ -8,6 +9,35 @@ def response(status_code, content_type, body):
     response += "\r\n"
     response += body
     return response
+
+
+def handle_request(client_socket):
+    data = client_socket.recv(1024)
+    if not data:
+        client_socket.close()
+        return
+    stringData = data.decode("ascii")
+    lines = stringData.split("\r\n")
+    verb, path, protocol = lines[0].split(" ")
+    headers = {}
+    for line in lines[1:]:
+        if line:
+            key, value = line.split(": ")
+            headers[key] = value
+
+    if verb == "GET":
+        if path == "/":
+            client_socket.send("HTTP/1.1 200 OK\r\n\r\nHello, World!".encode("ascii"))
+        elif path.startswith("/echo"):
+            #server_socket.send(f"HTTP/1.1 200 OK\r\n\r\n{path[6:]}".encode("ascii"))
+            client_socket.send(response("200 OK", "text/plain", path[6:]).encode("ascii"))
+        elif path.startswith("/user-agent"):
+            client_socket.send(response("200 OK", "text/plain", headers["User-Agent"]).encode("ascii"))
+        else:
+            client_socket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode("ascii"))
+    # server_socket.send("HTTP/1.1 200 OK\r\n\r\n".encode("ascii"))
+    client_socket.close()
+    
 
 
 def main():
@@ -22,32 +52,11 @@ def main():
 
 
     while True:
-        server_socket, config = server.accept()
+        client_socket, config = server.accept()
+        threading.Thread(target=handle_request, args=(client_socket,)).start()
         
-        data = server_socket.recv(1024)
-        if not data:
-            break
-        stringData = data.decode("ascii")
-        lines = stringData.split("\r\n")
-        verb, path, protocol = lines[0].split(" ")
-        headers = {}
-        for line in lines[1:]:
-            if line:
-                key, value = line.split(": ")
-                headers[key] = value
+        
 
-        if verb == "GET":
-            if path == "/":
-                server_socket.send("HTTP/1.1 200 OK\r\n\r\nHello, World!".encode("ascii"))
-            elif path.startswith("/echo"):
-                #server_socket.send(f"HTTP/1.1 200 OK\r\n\r\n{path[6:]}".encode("ascii"))
-                server_socket.send(response("200 OK", "text/plain", path[6:]).encode("ascii"))
-            elif path.startswith("/user-agent"):
-                server_socket.send(response("200 OK", "text/plain", headers["User-Agent"]).encode("ascii"))
-            else:
-                server_socket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode("ascii"))
-        # server_socket.send("HTTP/1.1 200 OK\r\n\r\n".encode("ascii"))
-        server_socket.close()
 
 
 if __name__ == "__main__":
