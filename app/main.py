8a58efbebf3078daa3561d43f461d06652f3ee40
Walter Miller <waltermil@gmail.com>
2024-02-15 19:34:26 -0500

    cleanup

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-# Uncomment this to pass the first stage
 import socket
 import threading
 import argparse
@@ -38,7 +37,6 @@ def handle_request(client_socket):
         if path == "/":
             client_socket.send("HTTP/1.1 200 OK\r\n\r\nHello, World!".encode("ascii"))
         elif path.startswith("/echo"):
-            #server_socket.send(f"HTTP/1.1 200 OK\r\n\r\n{path[6:]}".encode("ascii"))
             client_socket.send(response("200 OK", "text/plain", path[6:].encode("ascii")))
         elif path.startswith("/user-agent"):
             client_socket.send(response("200 OK", "text/plain", headers["User-Agent"].encode("ascii")))
@@ -59,21 +57,14 @@ def handle_request(client_socket):
             client_socket.send("HTTP/1.1 201 OK\r\n\r\n".encode("ascii"))
     else:
         client_socket.send(response_404())
-    # server_socket.send("HTTP/1.1 200 OK\r\n\r\n".encode("ascii"))
+
     client_socket.close()
     
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
     server = socket.create_server(("localhost", 4221), reuse_port=True)
     server.listen()
-    # server_socket.accept() # wait for client
-
 
     while True:
         client_socket, config = server.accept()
